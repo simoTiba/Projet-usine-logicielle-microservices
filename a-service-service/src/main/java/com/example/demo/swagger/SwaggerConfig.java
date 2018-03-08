effb27e81092aae92724d2bf4ebab9e83a87ff08
Nicolas Fostier <nicolasfost@gmail.com>
2018-03-08 20:48:03 +0100

    Swagger UI : correction du nom de package des APIs à exposer

--- a-service-service/src/main/java/com/example/demo/swagger/SwaggerConfig.java
+++ a-service-service/src/main/java/com/example/demo/swagger/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
 	
 	@Bean
 	public Docket ServiceApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.example.demo")).
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.example.demo.restcontroller")).
 				paths(PathSelectors.any()).build();
 				}
 		
