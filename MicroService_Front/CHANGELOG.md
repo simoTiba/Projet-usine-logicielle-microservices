2d92e65ec393e45187f3578cec205c5ea0f1d171
simo <tibasimo@gmail.com>
2018-03-09 15:04:57 +0100

    Last Version

--- MicroService_Front/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# CHANGE LOG
-## [1.0.1] 2017-12-07
--fixed errors for npm start
-## [1.0.0] 2017-06-27
-### Initial Release
--- MicroService_Front/src/assets/validForm.js
+++
@@ -1,7 +0,0 @@
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').focus()
-  });
-
-  $(document).ready(function() {
-    $('#example').DataTable();
-} );
